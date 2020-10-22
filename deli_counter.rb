d5407d284052afd9aa55835687b4db8ba88bee0e
Mike Alessi <mikealessi5@gmail.com>
2020-10-22 15:50:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-     katz_deli.each_with_index do |name, index|
+     katz_deli.each do |name, index|
        total_line.push("#{index+1}. #{name}")
      end
     puts "The line is currently: #{total_line.join(" ")}"
